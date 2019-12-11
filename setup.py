264a7bdfadab4502b4e69fbae41445f7c2269a15
Jasper Craeghs <jce@melexis.com>
2019-12-11 11:07:50 +0100

    Fix classifiers

--- setup.py
+++ setup.py
@@ -17,20 +17,13 @@ setup(
     long_description_content_type='text/x-rst',
     zip_safe=False,
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 4 - Beta',
         'Environment :: Console',
-
-        # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Packaging',
-
-        # Specify the Python versions you support here. In particular, ensure
-        # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3'
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Topic :: Documentation',
