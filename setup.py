899d29cd61314ace1358ac349b943fa3fedfa1e3
Jasper Craeghs <jasper.craeghs@gmail.com>
2022-06-22 12:29:23 +0200

    Set version range of setuptools-scm to keep support for Python 3.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     use_scm_version={
         'write_to': 'mlx/__xunit2rst_version__.py'
     },
-    setup_requires=['setuptools-scm'],
+    setup_requires=['setuptools-scm>=6.0.0,<7.*'],
     url='https://github.com/melexis/xunit2rst',
     license='Apache License Version 2.0',
     author='JasperCraeghs',
