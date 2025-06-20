075d2bd63ee9bd1481eb8afc5c4858eeefd91dca
Jasper Craeghs <jasper.craeghs@gmail.com>
2025-06-20 08:36:20 +0200

    Remove deprecated license specifier

--- setup.py
+++ setup.py
@@ -19,7 +19,6 @@ setup(
         'Development Status :: 5 - Production/Stable',
         'Environment :: Console',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
